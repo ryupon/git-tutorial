93e8cc9ea52ac8c72fde3d3eef91c3eb98085ee8
unknown <narita_r@tt.rim.or.jp>
2017-10-30 03:26:40 +0900

    Added Hello.java

---
+++ Hello.java
@@ -0,0 +1,21 @@
+
+class Hello
+{
+  public Hello()
+  {
+    
+  }
+
+  public void sayHello()
+  {
+    System.out.println("Hello World");
+  }
+
+  public static void main(String[] argv)
+  {
+    Hello app = new Hello();
+    app.sayHello();
+  }
+}
+
+
